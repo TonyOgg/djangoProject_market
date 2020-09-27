01030862149ba8478f72e7a2973a4a23900df3ab
TonyOgg <tony9808@mail.ru>
2020-09-27 16:18:26 +0300

    Django

--- Market/urls.py
+++ Market/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from Market.views import *
 
 urlpatterns = [
-    path('home', start)
+    path('home', start),
+    path('login', login_user),
+    path('start', main)
 ]
--- Market/views.py
+++ Market/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
 
 def start(request):
     return render(request, 'enter_page.html')
+
+def login_user(request):
+    context = {
+        'isin': request.user.is_authenticated
+    }
+    user = authenticate(
+        username=request.POST['username'],
+        password=request.POST['password']
+    )
+    if user is None:
+        return render(request, 'enter_page.html', context)
+    else:
+        login(request, user)
+        return HttpResponseRedirect('start')
+
+def main(request):
+    return render(request, 'main_page.html')
+
