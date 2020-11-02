fc2455f19e9f928336ebd649c5fd1dcf41508d9c
TonyOgg <tony9808@mail.ru>
2020-11-03 01:08:54 +0300

    Django

--- Market/static/js/logics_new_car_page.js
+++ Market/static/js/logics_new_car_page.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(".dropdown-menu li").click(function(){
 
-    $("#dropdownMenu5").text($(this).text());
-    $("#dropdownMenu5").val($(this).text());
+    $("#dropdownMenu1").text($(this).text());
+    $("#dropdownMenu1").val($(this).text());
   });
 })
