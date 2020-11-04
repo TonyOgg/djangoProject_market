a564f1222ad9c6196c570469b79c989c55dbc34e
TonyOgg <tony9808@mail.ru>
2020-11-05 01:40:36 +0300

    Django

--- Market/static/js/logics_new_car_page.js
+++ Market/static/js/logics_new_car_page.js
@@ -1,7 +1,42 @@
 $(document).ready(function () {
-    $(".dropdown-menu li").click(function(){
+    $("#dropdown1 li").click(function(){
 
     $("#dropdownMenu1").text($(this).text());
     $("#dropdownMenu1").val($(this).text());
   });
+    $("#dropdown2 li").click(function(){
+
+    $("#dropdownMenu2").text($(this).text());
+    $("#dropdownMenu2").val($(this).text());
+  });
+    $("#dropdown3 li").click(function(){
+
+    $("#dropdownMenu3").text($(this).text());
+    $("#dropdownMenu3").val($(this).text());
+  });
+    $("#dropdown4 li").click(function(){
+
+    $("#dropdownMenu4").text($(this).text());
+    $("#dropdownMenu4").val($(this).text());
+  });
+    $("#dropdown5 li").click(function(){
+
+    $("#dropdownMenu5").text($(this).text());
+    $("#dropdownMenu5").val($(this).text());
+  });
+    $("#dropdown6 li").click(function(){
+
+    $("#dropdownMenu6").text($(this).text());
+    $("#dropdownMenu6").val($(this).text());
+    });
+    $("#dropdown7 li").click(function(){
+
+    $("#dropdownMenu7").text($(this).text());
+    $("#dropdownMenu7").val($(this).text());
+    });
+    $("#dropdown8 li").click(function(){
+
+    $("#dropdownMenu8").text($(this).text());
+    $("#dropdownMenu8").val($(this).text());
+    });
 })
